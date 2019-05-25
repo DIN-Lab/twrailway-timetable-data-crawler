3af82462b3750897884a2601c0c29213f23bb489
Cheng-Yu Hsu <m@cyhsu.me>
2019-05-25 15:39:27 +0800

    Booleans are numberic in PTX API

--- train.py
+++ train.py
@@ -12,11 +12,11 @@ class TrainStop(MappableObject):
 class Train(MappableObject):
     _mappings = {
         'TrainNo' : string_mapper('number', base_key='TrainInfo'),
-        'BreastFeedFlag': boolean_mapper('has_breastfeeding_rooms', base_key='TrainInfo'),
-        'PackageServiceFlag' : boolean_mapper('carries_packages', base_key='TrainInfo'),
-        'DiningFlag' : boolean_mapper('has_dining_rooms', base_key='TrainInfo'),
-        'WheelChairFlag' : boolean_mapper('is_accessible', base_key='TrainInfo'),
-        'BikeFlag' : boolean_mapper('has_bike_stands', base_key='TrainInfo'),
+        'BreastFeedFlag': boolean_mapper('has_breastfeeding_rooms', is_numeric=True, base_key='TrainInfo'),
+        'PackageServiceFlag' : boolean_mapper('carries_packages', is_numeric=True, base_key='TrainInfo'),
+        'DiningFlag' : boolean_mapper('has_dining_rooms', is_numeric=True, base_key='TrainInfo'),
+        'WheelChairFlag' : boolean_mapper('is_accessible', is_numeric=True, base_key='TrainInfo'),
+        'BikeFlag' : boolean_mapper('has_bike_stands', is_numeric=True, base_key='TrainInfo'),
         'TrainTypeID' : string_mapper('train_class', base_key='TrainInfo'),
         'Direction': string_mapper('direction', base_key='TrainInfo'),
         'StopTimes': object_list_mapper('stops', TrainStop, 'order')
